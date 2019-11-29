890e5a8d8cd8f419be218731b1742e10bf668e4e
rantuki <woachxq@163.com>
2019-11-29 14:36:02 +0800

    webpackdevserver部分结束

---
+++ src/index.js
@@ -0,0 +1,1 @@
+console.log('hellowrold###22')
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: {
+    main: './src/index.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+      test: /\.(jpg|png|gif)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          // placeholder 占位符
+          name: '[name]_[hash].[ext]',
+          outputPath: 'images/',
+          limit: 2048
+        }
+      }
+    },{
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        {
+          'loader': 'css-loader',
+          options: {
+            importLoaders: 2
+          }
+        },
+        'sass-loader',
+        'postcss-loader'
+      ]
+    },{
+      test: /\.(eot|svg|ttf|woff)$/,
+      use: {
+        loader: 'file-loader'
+      }
+    }]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'src/index.html'
+  }), new CleanWebpackPlugin()],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
